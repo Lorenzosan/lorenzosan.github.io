510d0211ea745f1dc9ef8c20b8cabdcc5d8ddf60
Lorenzosan <lorenzo.sanfilippo@gmail.com>
2022-11-09 16:20:53 +0100

    Update MainPage.php

--- MainPage.php
+++ MainPage.php
@@ -1,15 +1,12 @@
 
-<?php
-
-$address="https://www.lorenzosanfilippo.com/";
-$url=$address;
-
-?>
-
 <html>
 
 <head>
  <title>This is a title</title>
 </head>
+ 
+ <body>
+ <?php echo '<p>Hello World</p>'; ?> 
+ </body>
   
 </html>
